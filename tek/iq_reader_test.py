39b112d30be3d502fe763dbae36530dbabe2a75b
Quan Guo <guotsuan@gmail.com>
2021-07-07 11:47:16 +0800

    Learn how to plot spectrogram

--- tek/iq_reader_test.py
+++ tek/iq_reader_test.py
@@ -18,13 +18,22 @@ import matplotlib
 from iqtools import *
 filename='data.tiq'
 dd = TIQData(filename)
-dd.read_samples(1024)
+dd.read_samples(512*8)
 ff, pp, _ = dd.get_fft()
+
 
 matplotlib.use("Qt5Agg")
 plt.plot(ff/1e6, 10*np.log10(pp/1e-3))
 dd.save_header()
 plt.show()
+plt.figure()
+# lframes: is the dimension for one fft
+# nframes: is the dimesion of time
+xx,yy,zz=dd.get_spectrogram(8, 512)
+plt.pcolor(xx, yy,zz)
+plt.show()
+
+
 
 # write_spectrum_to_root(ff, pp, filename, center=dd.center, title='spectrum')
 
